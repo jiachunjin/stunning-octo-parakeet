d74ee4a821aafa1f30d6a8c76eddd333b0b791af
Jiachun Jin <jiachun22.jin@gmail.com>
2025-09-20 13:41:47 +0800

    sync

--- runner/down_proj/joint_training.py
+++ runner/down_proj/joint_training.py
@@ -274,7 +274,7 @@ class MyTrainer(Trainer):
                     if self.global_step > 0 and self.global_step % self.config.train.save_every == 0 and self.accelerator.is_main_process:
                         self.model.eval()
                         state_dict = self.accelerator.unwrap_model(self.model).state_dict()
-                        save_path = os.path.join(self.config.train.output_dir, f"internvl-{self.config.train.exp_name}-{self.global_step}")
+                        save_path = os.path.join(self.output_dir, f"internvl-{self.config.train.exp_name}-{self.global_step}")
                         torch.save(state_dict, save_path)
                         print(f"internvl saved to {save_path}")
 
